d0fbbe88b27b0da114b73a8f1ddd1e1fbffbe984
johnchrishays <johnchrishays@gmail.com>
2019-12-31 13:53:01 -0500

    select lines from input

--- src/scripts/clean.sh
+++ src/scripts/clean.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+DATA=data/orig/tract_outcomes_early.csv 
 # Get list of column names that we care about
-
-csvcut -n data/orig/tract_outcomes_early.csv | # Extract the column names in <lineno>: <colname> fmt
+COLS=$(csvcut -n $DATA | # Extract the column names in <lineno>: <colname> fmt
 	cut -d ":" -f 2- | # Get any line that begins with `kfr` or `kir` 
 	cut -c 2- | # Remove first space
 	grep -E "^(kfr.*)|(kir.*)|(tract)|(state)|(county)" | # Get any line that begins with `kfr` or `kir`
-	tr '\n' , 
+	tr '\n' , | # Replace all newlines with commas
+	sed 's/.$//') # Remove the last comma
+
+csvcut -c $COLS $DATA | head -n 20
