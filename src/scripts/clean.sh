0fc5a2b0f06496b742bcf45ea56f47df15c37ba7
johnchrishays <johnchrishays@gmail.com>
2019-12-31 12:48:33 -0500

    get pipes working with comments

--- src/scripts/clean.sh
+++ src/scripts/clean.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Get list of column names that we care about
-csvcut -n data/orig/tract_outcomes_early.csv \ # Extract the column names in <lineno>: <colname> fmt
-	| cut -d ":" -f 2- \ # Get everything after the colon
-	| cut -c 2- \ # Remove first space
-	| grep -E "(kfr.*)|(kir.*)" # Get any line that begins with `kfr` or `kir`
+
+csvcut -n data/orig/tract_outcomes_early.csv | # Extract the column names in <lineno>: <colname> fmt
+	cut -d ":" -f 2- | # Get any line that begins with `kfr` or `kir` 
+	cut -c 2- | # Remove first space
+	grep -E "(kfr.*)|(kir.*)|(tract)|(state)|(county)" # Get any line that begins with `kfr` or `kir`
