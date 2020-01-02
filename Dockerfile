ea03ddcef04ff3e66faf786b5a669d43110b5d36
johnchrishays <johnchrishays@gmail.com>
2020-01-02 13:58:08 -0500

    add crosswalk from census tract -> school district

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,9 @@ RUN apk update && \
     apk add python3 && \
     apk add unzip && \
     pip3 install --upgrade pip && \
-    pip3 install csvkit
+    pip3 install -r src/python/requirements.txt 
 ADD https://opportunityinsights.org/wp-content/uploads/2018/10/tract_outcomes.zip $pwd/data/orig/
+ADD https://nces.ed.gov/programs/edge/data/GRF13.zip $pwd/data/orig/
 COPY ./src/ ./src/
 RUN chmod 755 ./src/scripts/clean.sh && \
     ./src/scripts/clean.sh
--- src/scripts/clean.sh
+++ src/scripts/clean.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# Unzip the tract outcomes data if doesn't exist
+if [ ! -e data/orig/tract_outcomes_early.csv ]
+  then 
+    echo "Unzipping tract_outcomes..."
+    unzip data/orig/tract_outcomes.zip -d data/orig/
+    echo "Done."
+fi
+
+# Unzip crosswalk from tracts to school districts if doesn't exist
+YR=19
+if [ ! -d data/orig/GRF${YR} ]
+  then 
+    echo "Unzipping GRF${YR}.zip..."
+    unzip data/orig/GRF${13}.zip -d data/orig/
+    echo "Done."
+fi
+
 DATA=data/orig/tract_outcomes_early.csv 
 # Get list of column names that we care about
 COLS=$(csvcut -n $DATA | # Extract the column names in <lineno>: <colname> fmt
 	cut -d ":" -f 2- | # Get any line that begins with `kfr` or `kir` 
 	cut -c 2- | # Remove first space
-	grep -E "^(kfr.*)|(kir.*)|(tract)|(state)|(county)" | # Get any line that begins with `kfr` or `kir`
+	grep -E "^(tract)|(state)|(county)" | # Get any line that begins with `kfr` or `kir`
 	tr '\n' , | # Replace all newlines with commas
 	sed 's/.$//') # Remove the last comma
 
